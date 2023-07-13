de25356a44c4c274aac15feebb6f7228b7ac8f74
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-07-13 22:58:34 +0430

    Add sign out link to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
 
   # Commented to prevent defaulting to sign in view to check if signed in or not
   devise_scope :user do
@@ -8,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :foods, except: [:update]
 
-  resources :users, only: [:index, :show]
+  resources :users
 
   get "/shopping_lists", to: "shopping_lists#index", as: "shopping_lists"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
