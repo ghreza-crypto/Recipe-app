55e99ba205a33dce910c1f5d956bb853d53eb564
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-07-13 22:35:59 +0430

    Add abiblity to user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,37 @@ class Ability
 
   def initialize(user)
     # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
+
+    return unless user.present?
+
+    if user.nil?
+      can :read, Food
+    else
+      can %i[read create], Food
+      can %i[read create], Recipe
+      can %i[read create], RecipeFood
+      can %i[update destroy], Food, user_id: user.id
+      can %i[update destroy], Recipe, user_id: user.id
+      can %i[update destroy], RecipeFood, user_id: user.id
+    end
+
+    # <% if can? :manage, @question %>
+    #   <%= link_to 'Edit', edit_question_path(@question) %> | <%= link_to 'Destroy', @question, method: :delete, data: { confirm: 'Are you sure you want to delete this job?' } %>
+    # <% end %>
+
+    # return unless user.present?
+
+    # can :read, :all
+    # can :create, [Food, Recipe, RecipeFood]
+    # can :destroy, [Food, Recipe, RecipeFood]
+
+    # if user.role == 'admin'
+    #   can :manage, :all
+    # else
+    #   can :destroy, Food, author: user
+    #   can :destroy, Recipe, author: user
+    #   can :destroy, RecipeFood, author: user
+    # end
     #   can :read, :all
     #   return unless user.admin?
     #   can :manage, :all
