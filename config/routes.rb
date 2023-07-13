22bbf646e1492cb7e0eb028fe8173be5e7e6c2d2
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-07-13 20:57:09 +0430

    Add food routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # Commented to prevent defaulting to sign in view to check if signed in or not
+  devise_scope :user do
+    root "devise/sessions#new"
+  end
+
+  resources :foods, except: [:update]
+
+  resources :users, only: [:index, :show]
+
+  get "/shopping_lists", to: "shopping_lists#index", as: "shopping_lists"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
 end
